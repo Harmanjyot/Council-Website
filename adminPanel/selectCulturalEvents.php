61318d397b7a01ad31b28068cf2af1037f9ff601
vedpatil611 <vedpatil611@gmail.com>
2019-08-11 10:46:02 +0530

    Add image to cultural events

--- adminPanel/selectCulturalEvents.php
+++ adminPanel/selectCulturalEvents.php
@@ -11,12 +11,14 @@ $output .= '
     <div class="table-responsive">
         <table class="table table-bordered">
             <tr>
-                <th width="10%">Sr. No</th>
-                <th width="20%">Event Name</th>
-                <th width="15%">Event Date</th>
-				<th width="15%">Event Time</th>
-                <th width="30%">Event Description</th>
-                <th width="10%">Delete</th>
+                <th width="10%">Sr. No</th>  
+                     <th width="20%">Event Name</th>  
+                     <th width="15%">Event Date</th>  
+                     <th width="15%">Event Time</th>  
+                     <th width="30%">Event Description</th> 
+                     <th width="50%">Event Image</th>
+                     <th width="50%">Image Preview</th>
+                     <th width="10%">Delete</th>
                 </tr>';
 if(mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_array($result)) {  
@@ -28,6 +30,8 @@ if(mysqli_num_rows($result) > 0) {
     	        	<td><input type="text" size="10" value="'.$row["eventDate"].'" style="border: none" id="col2" readonly></td>
 	    	        <td><input type="text" size="10" value="'.$row["eventTime"].'" style="border: none" id="col3" readonly></td>
     	    	    <td><input type="text" value="'.$row["eventDescription"].'" style="border: none" id="col4" readonly></td>
+    	    	    <td><button type="button" name="update" class="btn btn-warning bt-xs update" id="'.$row["SrNo"].'">Upload</button></td>
+    	    	    <td><img src="data:image/jpeg;base64,'.base64_encode($row['eventImage'] ).'" height="60" width="75"/></td>
     	            <td><input type="submit" name="delete_btn" data-id5="'.$row["SrNo"].'" class="btn btn-xs btn-danger btn_delete" value="x"></td>
                 </tr>  
             </form>
@@ -38,6 +42,8 @@ if(mysqli_num_rows($result) > 0) {
                     <td><input type="text" size="10" name="cultural_date" value="'.$row["eventDate"].'" style="border: none" id="col2"></td>
                     <td><input type="text" size="10" name="cultural_time" value="'.$row["eventTime"].'" style="border: none" id="col3"></td>
                     <td><input type="text" name="cultural_desc" value="'.$row["eventDescription"].'" style="border: none" id="col4"></td>
+                    <td><button type="button" name="update" class="btn btn-warning bt-xs update" id="'.$row["SrNo"].'">Upload</button></td>
+    	    	    <td><img src="data:image/jpeg;base64,'.base64_encode($row['eventImage'] ).'" height="60" width="75"/></td>
                     <td><input type="submit" name="delete_btn" data-id5="'.$row["SrNo"].'" class="btn btn-xs btn-danger btn_delete" value="Save"></td>
                 </tr>  
             </form>';  
@@ -68,10 +74,76 @@ if(mysqli_num_rows($result) > 0) {
 		</form>';  
 }  
 $output .= '</table></div>';
+$output .= '
+ <div id="imageModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+   <div class="modal-content">
+    <div class="modal-header">
+     <button type="button" class="close" data-dismiss="modal">&times;</button>
+     <h4 class="modal-title">Add Image</h4>
+    </div>
+    <div class="modal-body">
+     <form id="image_form" method="post" enctype="multipart/form-data">
+      <p><label>Select Image</label>
+      <input type="file" name="image" id="image" /></p><br />
+      <input type="hidden" name="action" id="action" value="insert" />
+      <input type="hidden" name="image_id" id="image_id" />
+      <input type="submit" name="insert" id="insert" value="Insert" class="btn btn-info" />
+     </form>
+    </div>
+    <div class="modal-footer">
+     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+    </div>
+   </div>
+  </div>
+ </div>';
 echo $output;
 ?>
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+<script src="js/plugins/pace.min.js"></script>
 
 <script type="text/javascript">
+	// $(document).ready(function(){  
+	   	$(".update").click(function(){
+    	    $('#image_id').val($(this).attr("id"));
+        	$('#action').val("update");
+	        $('.modal-title').text("Update Image");
+    	    $('#insert').val("Update");
+        	$('#imageModal').modal("show");
+		});
+	   	$('#image_form').submit(function(event) {
+    	    event.preventDefault();
+        	var image_name = $('#image').val();
+	        if(image_name == '') {
+    	        alert("Please Select Image");
+        	    return false;
+	        } else {
+		        var extension = $('#image').val().split('.').pop().toLowerCase();
+    		    if(jQuery.inArray(extension, ['gif','png','jpg','jpeg']) == -1) {
+        		    alert("Invalid Image File");
+            		$('#image').val('');
+		            return false;
+    		    } else {
+        		    $.ajax({
+            		    url:"uploadCulturalImage.php",
+                		method:"POST",
+	                	data:new FormData(this),
+	 		            contentType:false,
+    	 		        processData:false,
+        	 		    success:function(data) {
+		    	    	    alert(data);
+        			    	fetch_data();
+			        	    $('#image_form')[0].reset();
+    	    		    	$('#imageModal').modal('hide');
+	           			}
+		        	});
+        		}
+	   		}
+ 		});
+    // });
     document.getElementById('Cancel').style.visibility = "hidden";
     var editlist = document.getElementsByClassName('editThis');
     for(i=0;i<editlist.length;i++) {
---
+++ adminPanel/uploadCulturalImage.php
@@ -0,0 +1,14 @@
+<?php
+
+include '../php/conn.php';
+if($_POST["action"] == "update")
+ {
+  $file = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));
+  $query = "UPDATE culturaleventlist SET eventImage ='".$file."' WHERE SrNo='".$_POST["image_id"]."'";
+  if(mysqli_query($conn, $query))
+  {
+   echo 'Image Updated into Database';
+  }
+ }
+
+?>
