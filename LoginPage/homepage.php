9f4c8e03dca71e8c1cc7f318c85711d53eb43b82
Harmanjyot <37540756+Harmanjyot@users.noreply.github.com>
2019-08-15 22:29:03 +0530

    q

--- LoginPage/homepage.php
+++ LoginPage/homepage.php
@@ -828,7 +828,7 @@ the most magnificent three days that you can enjoy to your heart&#39;s content! 
 Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 
-                <div class="footer-social">
+<!--                 <div class="footer-social">
                     <ul class="flex flex-wrap justify-content-center align-items-center">
                         <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
                         <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
@@ -836,7 +836,7 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                         <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                         <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                     </ul>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
