3a7fe36fae4af43a4d7a59928d522de2e4a4c53b
Harmanjyot <37540756+Harmanjyot@users.noreply.github.com>
2019-08-18 22:06:58 +0530

    final database

--- loginAdmin/login.php
+++ loginAdmin/login.php
@@ -11,26 +11,20 @@ $stmt = mysqli_stmt_init($conn);
 mysqli_stmt_prepare($stmt, $sql);
 mysqli_stmt_bind_param($stmt,"s", $login);
 mysqli_stmt_execute($stmt);
-echo "SECOND";
-$result = mysqli_stmt_get_result($stmt);
-echo "THREE";			
+$result = mysqli_stmt_get_result($stmt);		
 			if (mysqli_num_rows($result) > 0) {
 				$row = mysqli_fetch_assoc($result);
-				echo "FOUR";
-				echo $row["login"];
 				$pwdCheck = password_verify($password, $row['password']);
 				if ($pwdCheck == false) {
-					echo "false";
 					exit();
 				}
 				else if ($pwdCheck == true) {
-					echo "this is here";
 					session_start();
-					$_SESSION['userId'] = $row['login'];
+					$_SESSION['userType'] = $row['login'];
 					// $_SESSION['userRoll'] = $row['studentRoll'];
 					if($login == "admin") {
 						header("Location: ../adminPanel/dashboard.php?login=success");
-					} else {
+					} elseif ($login == "teacher") {
 						header("Location: ../teacherPanel/dashboard.php?login=success");
 					}
 					exit();
